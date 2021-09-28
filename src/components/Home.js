85558224d6f9cd6daffac4a99695bf80af0d9ace
roco sittoni <roco.sittoni@protonmail.com>
2021-09-27 21:23:03 -0300

    display home image and fetch movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // Config 
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // Components
+import HeroImage from './HeroImage';
+import Grid from "./Grid";
 
 // Hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-    const [state, setState] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    
+    const {state, loading, error} = useHomeFetch();
 
-    return <div>Home Page</div>
-}
+    console.log(state);
+
+    return (
+        <>
+            { state.results[0] ? (
+            <HeroImage 
+                image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title={state.results[0].original_title}
+                text={state.results[0].overview}
+            />
+            ) : null }
+            <Grid header='Popular Movies'>
+                {state.results.map(movie => (
+                    <div key={movie.id}>{movie.title}</div>
+                ))}
+            </Grid>
+        </>
+    )
+};
 
 export default Home;
